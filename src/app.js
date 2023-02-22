10c70c3c20775a91e72b621db12a15a3bdac3baf
Aaninat <aaninat90@gmail.com>
2023-02-22 22:23:58 +0000

    ejercicio resuelto

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (var i = 0; i < pronoun.length; i++) {
+  for (var j = 0; j < adj.length; j++) {
+    for (var k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
+}
